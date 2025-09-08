3af86492e3a658d9ec7d602fc385d4ec1d6fe150
Arthur Fellipe <arthur.fellipe.brandao@gmail.com>
2025-09-08 17:03:06 -0300

    update

--- app/boypage/page.tsx
+++ app/boypage/page.tsx
@@ -3,76 +3,111 @@
 import React from 'react';
 import Header from '../components/header/page';
 import Contain from '@/app/components/contain/page';
-import SlipperAsunaAllBlack from "@/app/public/imgCalçados/ChineloAsunaAllBlack.jpg"
-import SlipperAsunaBege from "@/app/public/imgCalçados/ChineloAsunabege.jpg"
-import SlliperAsunaCinza from "@/app/public/imgCalçados/ChineloAsunaCinza.jpg"
-import SlliperAsunaPreto from '@/app/public/imgCalçados/ChineloAsunaPreto.jpg'
+import SlipperAsunaAllBlack from "@/app/public/imgCalçados/ChineloAsunaAllBlack.jpg";
+import SlipperAsunaBeige from "@/app/public/imgCalçados/ChineloAsunabege.jpg";
+import SlipperAsunaGray from "@/app/public/imgCalçados/ChineloAsunaCinza.jpg";
+import SlipperAsunaBlack from '@/app/public/imgCalçados/ChineloAsunaPreto.jpg';
+import SneakersVansAllBlack from '@/app/public/imgCalçados/TenisVansAllBlack.jpg';
+import SneakersVansAllWhite from '@/app/public/imgCalçados/TenisVansAllWhite.jpg';
+import SneakersVansBrown from '@/app/public/imgCalçados/TenisVansBrown.jpg';
+import SneakersVansBlack from '@/app/public/imgCalçados/TenisVansBlack.jpg';
+import SneakersAirForceAllBlack from '@/app/public/imgCalçados/TênisNikeAirForceLinhaPremiumBlack.jpg';
+import SneakersAirForceAllWhite from '@/app/public/imgCalçados/TênisNikeAirForceLinhaPremiumWhite.jpg';
+import SneakersAirForceLinhaPremiumstringBlack from '@/app/public/imgCalçados/Tênis NikeAirForceLinhaPremiumCordaPreta.jpg'
+import SneakersAirForceLinhaPremiumstringWhite from '@/app/public/imgCalçados/Tênis NikeAirForceLinhaPremiumCordaBranca.jpg';
 
 
-
-// Dados dos produtos
-const sneakersHugoBoss = [
+// Product data
+const AsunaSlippers = [
   {
     src: SlipperAsunaAllBlack.src,
-    alt: ''
+    alt: 'Asuna Slipper'
   },
   {
-    src: SlipperAsunaBege.src,
-    alt: ''
+    src: SlipperAsunaBeige.src,
+    alt: 'Asuna Slipper'
   },
   {
-    src: 
-  }
+    src: SlipperAsunaGray.src,
+    alt: 'Asuna Slipper'
+  },
+  {
+    src: SlipperAsunaBlack.src,
+    alt: 'Asuna Slipper'
+  }   
 ];
 
+const VansSneakers = [
+  {
+    src: SneakersVansAllBlack.src,
+    alt: 'Vans VR3 Neo All Black Sneakers'
+  },
+  {
+    src: SneakersVansAllWhite.src,
+    alt: 'Vans VR3 Neo White Sneakers'
+  },
+  {
+    src: SneakersVansBlack.src,
+    alt: 'Vans VR3 Neo Black Sneakers'
+  },
+  {
+    src: SneakersVansBrown.src,
+    alt: 'Vans VR3 Neo Brown Sneakers'
+  }
 
-// Página principal
+];
+
+const AirForceSneakers = [
+  {
+    src: SneakersAirForceAllBlack.src,
+    alt: 'Nike Air Force All Black',
+  },
+  {
+    src: SneakersAirForceAllWhite.src,
+    alt: 'Nike Air Force White',
+  },
+  {
+    src: SneakersAirForceLinhaPremiumstringBlack.src,
+    alt: 'Nike Air Force Black with String',
+  },
+  {
+    src: SneakersAirForceLinhaPremiumstringWhite.src,
+    alt: 'Nike Air Force White with String',
+  }
+  
+]
+
+// Main page
 export default function BoyPage() {
   return (
     <div>
       {/* Header */}
       <Header />
 
-      {/* Divisor */}
+      {/* Divider */}
       <div className="w-full h-[1px] bg-gray-500"></div>
 
-      {/* Conteúdo principal */}
+      {/* Main content */}
       <div className="min-h-screen bg-black p-8">
         <main className="flex gap-2 items-center justify-center">
           <Contain
-            sneakers={sneakersHugoBoss}
-            title="Tênis Hugo Boss"
-            price="R$ 199,99"
+            sneakers={AsunaSlippers}
+            title="Asuna Slipper Premium Line"
+            price="R$ 150,00"
           />
           <Contain
-            sneakers={sneakersArmani}
-            title="Tênis Armani"
-            price="R$ 299,99"
+            sneakers={VansSneakers}
+            title="Vans VR3 Sneakers"
+            price="R$ 100,00"
           />
           <Contain
-          sneakers={sneakersBoss}
-          title="Tênis Boss"
-          price="R$ 129,99"
+            sneakers={AirForceSneakers}
+            title="Nike Air Force Premium Line"
+            price="R$ 86,00"
           />
         </main>
 
-        <main className='flex gap-2 items-center justify-center'>
-          <Contain
-            sneakers={sneakersLeather}
-            title="Tênis Hugo Boss"
-            price="R$ 199,99"
-          />
-
-          <Contain
-            sneakers={sneakersNewBalance}
-            title="Tênis New Balance"
-            price="R$ 299,99"
-          />
-          <Contain
-            sneakers={sneakersReserva}
-            title="Tênis Reserva"
-            price="R$ 129,99"
-          />
+        <main className="flex gap-2 items-center justify-center">
         </main>
       </div>
     </div>
--- app/components/contain/page.tsx
+++ app/components/contain/page.tsx
@@ -79,7 +79,7 @@ export default function Contain({ sneakers, title, price }: ContainProps) {
       </figure>
 
       {/* Título e Preço */}
-      <h2 className="mt-8 text-lg font-semibold">{title}</h2>
+      <h2 className="mt-[3rem] text-lg font-semibold">{title}</h2>
       <p className="text-xl font-thin">{price} no Pix</p>
       <p className="text-sm mt-2">ou até 6x sem juros</p>
     </article>
